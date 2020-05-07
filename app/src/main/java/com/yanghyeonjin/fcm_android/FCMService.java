5e95876b67b90dd98353b60dbeec60521e213ca6
yanghyeonjin <guswls7081@gmail.com>
2020-05-07 15:16:35 +0900

    FCM Service 등록

---
+++ app/src/main/java/com/yanghyeonjin/fcm_android/FCMService.java
@@ -0,0 +1,30 @@
+package com.yanghyeonjin.fcm_android;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FCMService extends FirebaseMessagingService {
+    private static final String TAG = "FCMService";
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        /**
+         * 새 토큰이 생성될 때마다 onNewToken 콜백이 호출됩니다.
+         *
+         * 다음과 같은 경우에 등록 토큰이 변경될 수 있습니다.
+         * - 앱에서 인스턴스 ID 삭제
+         * - 새 기기에서 앱 복원
+         * - 사용자가 앱 삭제/재설치
+         * - 사용자가 앱 데이터 소거
+         */
+        Log.e(TAG, "Refreshed token: " + s);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        // sendRegistrationToServer(token);
+    }
+}
